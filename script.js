e11188d70927fc745caf7d6d0cc7107c7d97ab2a
Marcellino-cell <fahrifajar379@gmail.com>
2026-03-13 14:03:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -249,4 +249,26 @@ alert("Gagal mengirim email")
 
 })
 
+function toggleBrakeSection(){
+
+let value = document.getElementById("motorBrakeSelect").value
+
+let smiRow = document.getElementById("smiRow")
+let encoderRow = document.getElementById("encoderRow")
+
+if(value === "no"){
+
+smiRow.style.display = "none"
+encoderRow.style.display = "none"
+
+}else{
+
+smiRow.style.display = "table-row"
+encoderRow.style.display = "table-row"
+
 }
+
+}
+
+}
+
