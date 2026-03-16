2bf66d44688be6d44b3b0e2e4dc614e16ecb3160
Marcellino-cell <fahrifajar379@gmail.com>
2026-03-16 13:00:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ const pageHeight = 297
 
 const imgHeight = canvas.height * imgWidth / canvas.width
 
-pdf.addImage(imgData,'PNG',0,0,imgWidth,imgHeight)
+pdf.addImage(imgData,'PNG',0,0,210,297)
 
 pdf.save("CNC_Service_Report.pdf")
 
